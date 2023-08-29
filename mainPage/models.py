c4572db081c2e06f42cab95645fe7888af12f275
ArdahanTemel <ardahantemel@gmail.com>
2023-08-30 00:46:06 +0300

    20230830-1

--- mainPage/models.py
+++ mainPage/models.py
@@ -6,27 +6,23 @@ from . import helperFunctions
 # Create your models here.
 
 class Tedarikci(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Ad")
-    surname = models.CharField(max_length=50, verbose_name="Soyad")
-    idNumber = models.CharField(max_length=25, verbose_name="TC Kimlik No")
-    adress = models.CharField(max_length=200, verbose_name="Adres")
-    tax = models.CharField(max_length=100, verbose_name="Vergi Dairesi")
-    mail1 = models.EmailField(max_length=100, verbose_name="E-mail 1")
-    mail2 = models.EmailField(max_length=100, verbose_name="E-mail 2")
-    phoneNumber1 = models.CharField(max_length=20, verbose_name="Telefon 1")
-    phoneNumber2 = models.CharField(max_length=20, verbose_name="Telefon 2")
-    IBAN = models.CharField(max_length=100, verbose_name="IBAN")
-    bankNumber = models.CharField(max_length=100, verbose_name="Banka Hesap Numarası")
-    bank = models.CharField(max_length=100, verbose_name="Banka")
-    companyName = models.CharField(max_length=100, verbose_name="Şirket")
+    name = models.CharField(max_length=50, verbose_name="Ad Soyad / Şirket")
+    idNumber = models.CharField(max_length=25, verbose_name="TC Kimlik No / Vergi No",null=True,blank=True)
+    adress = models.CharField(max_length=200, verbose_name="Adres",null=True,blank=True)
+    tax = models.CharField(max_length=100, verbose_name="Vergi Dairesi",null=True,blank=True)
+    mail1 = models.EmailField(max_length=100, verbose_name="E-mail 1",null=True,blank=True)
+    mail2 = models.EmailField(max_length=100, verbose_name="E-mail 2",null=True,blank=True)
+    phoneNumber1 = models.CharField(max_length=20, verbose_name="Telefon 1",null=True,blank=True)
+    phoneNumber2 = models.CharField(max_length=20, verbose_name="Telefon 2",null=True,blank=True)
+    IBAN = models.CharField(max_length=100, verbose_name="IBAN",null=True,blank=True)
 
     def __str__(self):
-        return self.name + " " + self.surname + " / " + self.companyName
+        return self.name
 
 
 class Hammadde(models.Model):
     rawMaterialName = models.CharField(max_length=200, verbose_name="Hammadde Tipi")
-    isScrap = models.BooleanField(verbose_name="Değersiz / Hurda mı?", choices=[(1, 'EVET'), (0, 'HAYIR')])
+    category=models.CharField(max_length=50,verbose_name="Mal Kategorisi",null=True,blank=True)
 
     def __str__(self):
         return self.rawMaterialName
